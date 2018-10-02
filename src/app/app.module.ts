a5ed328c0d097d186854d1b67c3631931107957b
Carmelo Huesca <carmelo.huesca.sge@mango.com>
2018-10-02 12:03:16 +0200

    create the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from '@angular/flex-layout';
 import { HeaderModule } from './components/header';
+import { LandingComponent } from './modules/landing/landing.component';
+import { LandingModule } from './modules/landing/landing.module';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: LandingComponent
   }
 ];
 @NgModule({
@@ -23,6 +29,7 @@ const routes: Routes = [
     BrowserModule,
     CoreModule,
     HeaderModule,
+    LandingModule,
     BrowserAnimationsModule
   ],
   providers: [],
